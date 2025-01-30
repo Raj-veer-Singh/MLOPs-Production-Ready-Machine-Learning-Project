8f9f06afd827cb107aad7d103731460897e29bd7
Raj-veer-Singh <you@example.com>
2025-01-30 14:43:11 +0530

    updated setups

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Rajveer",
+    author_email="raj8828295131@gmail.com",
+    packages=find_packages()
+)
